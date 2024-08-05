2a4e0a6afaa5a4e1809bb1524ea8241b85836d57
xenomorpheus <xenomorpheus@users.noreply.github.com>
2024-08-05 18:39:37 +0100

    Log messages improved

--- src/main/java/com/example/dynamictree/TreePopulatorDelay.java
+++ src/main/java/com/example/dynamictree/TreePopulatorDelay.java
@@ -22,23 +22,27 @@ public class TreePopulatorDelay extends Thread {
 	@Override
 	public void run() {
 		logger.info("Sleeper must awaken");
-		DefaultMutableTreeNode child1 = dynamicTreePanel.addObject(null, "child1 - remove by dynamicTreePanel", true);
-		DefaultMutableTreeNode child2 = dynamicTreePanel.addObject(null, "child2 - remove from Parent Node", true);
+		logger.info("Adding child1");
+		DefaultMutableTreeNode child1 = dynamicTreePanel.addObject(null, "child1 - To be removed by dynamicTreePanel", true);
+		logger.info("Adding child2");
+		DefaultMutableTreeNode child2 = dynamicTreePanel.addObject(null, "child2 - To be removed from Parent Node", true);
 		try {
 			Thread.sleep(DELAY);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		logger.info("Removing child1 from parent");
 		dynamicTreePanel.removeNode(child1);
-		logger.info("Child1 removed by dynamicTree - Successfully updates UI");
+		logger.info("Completed child1 removed by dynamicTree - Successfully updates UI");
 		try {
 			Thread.sleep(DELAY);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		logger.info("Removing child2 from parent");
 		child2.removeFromParent();
-		logger.info("MISSING - treeNodesRemoved called on TreeModelEvent");
-		logger.info("Child2 removed by removeFromParent - Fails to update UI");
+		logger.info("ERROR: MISSING notification - treeNodesRemoved called on TreeModelEvent");
+		logger.info("ERROR: Completed child2 removed by removeFromParent - Fails to update UI");
 		try {
 			Thread.sleep(DELAY);
 		} catch (InterruptedException e) {
