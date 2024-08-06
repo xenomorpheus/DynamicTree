0d2dbcab3dd0c74efda8c91c16923f09fa98a4bb
xenomorpheus <xenomorpheus@users.noreply.github.com>
2024-08-06 22:09:12 +0100

    Better description of errors

--- README.md
+++ README.md
@@ -12,3 +12,17 @@ Run
 ===
 
 java -jar target/DynamicTree-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+
+
+Correct Behaviour
+=================
+
+The JTree will be correctly updated when adding or removing nodes by directly calling methods on the DynamicTreePanel.
+
+Faults
+======
+
+The JTree fails to be updated when adding/removing nodes to/from existing nodes.
+
+The MyTreeModelListener instance fails to be notified of changes to the DefaultTreeModel.
+
--- src/main/java/com/example/dynamictree/TreeManipulator.java
+++ src/main/java/com/example/dynamictree/TreeManipulator.java
@@ -23,15 +23,15 @@ public class TreeManipulator extends Thread {
 	public void run() {
 		logger.info("TreeManipulator starting");
 		logger.info("Adding child1");
-		DefaultMutableTreeNode child1 = dynamicTreePanel.addObject(null, "child1 - To be removed by dynamicTreePanel", true);
+		DefaultMutableTreeNode child1 = dynamicTreePanel.addObject(null, "child1 - To be removed by method removeNode()", true);
 		logger.info("Adding child2");
-		DefaultMutableTreeNode child2 = dynamicTreePanel.addObject(null, "child2 - To be removed from Parent Node", true);
+		DefaultMutableTreeNode child2 = dynamicTreePanel.addObject(null, "child2 - To be removed by method removeFromParent()", true);
 		try {
 			Thread.sleep(DELAY);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		logger.info("Removing child1 from parent");
+		logger.info("Removing child1 by method removeNode()");
 		dynamicTreePanel.removeNode(child1);
 		logger.info("Completed child1 removed by dynamicTree - Successfully updates UI");
 		try {
@@ -39,8 +39,10 @@ public class TreeManipulator extends Thread {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		logger.info("Removing child2 from parent");
+
+		logger.info("Removing child2 by method removeFromParent()");
 		child2.removeFromParent();
+
 		logger.info("ERROR: MISSING notification - treeNodesRemoved called on TreeModelEvent");
 		logger.info("ERROR: Completed child2 removed by removeFromParent - Fails to update UI");
 		try {
